905b0f37a7561668ccbb65e412109640498f4e01
Kartopelka-swift <olehpalii@icloud.com>
2026-06-07 14:21:32 +0200

    Update StateCounter.swift

--- 01-state/StateCounter.swift
+++ 01-state/StateCounter.swift
@@ -1,1 +1,21 @@
+import SwiftUI
 
+struct StateCounter: View {
+    @State private var count: Int = 0
+    
+    var body: some View {
+        VStack {
+            Text("mini counter :)")
+            Spacer(4)
+            Text(count)
+            HStack {
+                Button("+1"action: {count += 1})
+                Button("-1"action: {count -= 1})
+            }
+            Spacer()
+            
+        }
+    }
+
+
+}
