8c331691b69fb559def8f84e40ae1af4a6ced69c
Kartopelka-swift <olehpalii@icloud.com>
2026-06-09 02:30:23 +0200

    Update ClosureCalculator.swift

--- 02-closures/ClosureCalculator.swift
+++ 02-closures/ClosureCalculator.swift
@@ -1,1 +1,20 @@
+import SwiftUI
 
+struct ClosureCalculator: View {
+@State private var result = 0
+
+    var body: some View {
+        let add = { (a: Int, b: Int) -> Int in
+            return a + b
+        }
+
+        HStack{
+            Text("3 + 5")
+            Button("=") {
+                result = add(3,5)
+            }
+            Text(result)
+        }
+       
+    }
+}
